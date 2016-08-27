84d915487ba86b67be61c87830ca4eb1d3525974
Freebios <piratequentin@gmail.com>
2016-08-27 15:15:28 +0200

    Animation

---
+++ src/fr/ld32/render/Animation.java
@@ -0,0 +1,107 @@
+package fr.ld32.render;
+
+import org.lwjgl.opengl.GL11;
+
+import fr.mimus.jbasicgl.graphics.Mesh;
+import fr.mimus.jbasicgl.graphics.Shaders;
+import fr.mimus.jbasicgl.graphics.Texture;
+import fr.mimus.jbasicgl.maths.Mat4;
+import fr.mimus.jbasicgl.maths.Vec2;
+
+public class Animation {
+	int columns, lines, width, height;
+	int start, end;
+	
+	Texture tex;
+	
+	Mesh m;
+	boolean isPaused = false, 
+			isFlipped = false,
+			isReversed = false;
+	int frame;
+	float delay;
+	
+	long lastFrame;
+	
+	public Animation(int c, int w, Texture texture, float secDelay){
+		this(c, 1, w, w, texture, 0, c-1, secDelay);
+	}
+	
+	public Animation(int c, int w, Texture texture, int s, int e, float secDelay){
+		this(c, 1, w, w, texture, s, e, secDelay);
+	}
+	
+	public Animation(int c, int l, int w, int h, Texture texture, int s, int e, float secDelay){
+		columns = c;
+		lines = l;
+		width = w;
+		height = h;
+		start = s;
+		end = e;
+		
+		tex = texture;
+		
+		delay = secDelay;
+		
+		m = new Mesh(4);
+		m.addVertices(0,0).addTexCoord2f(0, 0);
+		m.addVertices(width,0).addTexCoord2f(1f / columns, 0);
+		m.addVertices(width,height).addTexCoord2f(1f / columns, 1f / lines);
+		m.addVertices(0,height).addTexCoord2f(0, 1f / lines);
+		
+		m.buffering();
+	}
+	
+	public void render(Shaders shader, Vec2 pos){
+		shader.setUniformMat4f("m_view", Mat4.multiply(Mat4.translate(pos), Mat4.scale(isFlipped?-1:1, 1, 1)));
+		shader.setUniform1f("anim", (float) frame / columns);
+		
+		if(tex != null){
+			tex.bind();
+			m.render(GL11.GL_QUADS);
+			Texture.unbind();
+		}
+	}
+	
+	public void update(){
+		if(frame > end) frame = start;
+		if(frame < start) frame = end;
+		if(System.currentTimeMillis() - lastFrame > delay * 1000)
+		{
+			frame += isReversed?-1:1;
+			lastFrame = System.currentTimeMillis();
+		}
+	}
+	
+	public void togglePause() {
+		isPaused = !isPaused;
+	}
+	
+	public void toggleFlip() {
+		isFlipped = !isFlipped;
+	}
+	
+	public void toggleReverse() {
+		isReversed = !isReversed;
+	}
+	
+	public void setPause(boolean bool) {
+		isPaused = bool;
+	}
+	
+	public void setFlip(boolean bool) {
+		isFlipped = bool;
+	}
+	
+	public void setReverse(boolean bool) {
+		isReversed = bool;
+	}
+	
+	public void setDelay(float d){
+		delay = d;
+	}
+	
+	public int getFrame(){
+		return frame;
+	}
+}
